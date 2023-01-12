e50df2143c4e4c044bc0f55e403777adaee2e55e
mr-erold <mmpterold@gmail.com>
2023-01-12 14:50:24 +0200

    Add Cors configuration.

---
+++ d66-social/src/main/java/com/eroldmr/d66/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.eroldmr.d66.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+/**
+ * @author Mompati 'Patco' Keetile
+ */
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.setAllowCredentials(true);
+    corsConfiguration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:4200"));
+    corsConfiguration.setAllowedHeaders(List.of("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept",
+            "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request-Method",
+            "Access-Control-Request-Headers"));
+    corsConfiguration.setExposedHeaders(List.of("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+            "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Filename"));
+    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+    urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+    return new CorsFilter(urlBasedCorsConfigurationSource);
+  }
+}
